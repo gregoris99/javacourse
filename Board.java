abc216a755cac7190e65e9ba0bf70333e31de184
gregoris99 <gregoris50074@gmail.com>
2019-03-16 14:41:06 +0000

    Highlighting Valid Moves

--- Board.java
+++ Board.java
@@ -2,13 +2,30 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Board {
+public class Board implements ActionListener
+{
     JFrame frame = new JFrame("Java Coursework");
     JPanel panel = new JPanel();
     GridLayout layout = new GridLayout(8, 8, 0, 0);
     ImageIcon red = new ImageIcon("red.png");
     ImageIcon white = new ImageIcon("white.png");
+    ImageIcon valid = new ImageIcon("selected.png");
+
+     //ARRAY OF INSTANCES CREATED
+
+     Square[] squares = new Square[65]; 
+
+     //2D ARRAY OF BUTTONS
+
+     JButton[][] blocks= new JButton[65][2];   
+
+    public void setSelected(JButton x)
+    {
+      x.setIcon(valid);
+    }
     
+    //CONSTRUCTOR
+    //ADDING ACTIONLISTENERS TO ALL WHITE BUTTONS    
 
     public Board()
     {
@@ -18,13 +35,10 @@ public class Board {
         frame.setContentPane(panel);
         frame.setVisible(true);
         panel.setLayout(layout); 
-
-        Square[] squares = new Square[65]; //array of instances created
-        
-        JButton[][] blocks= new JButton[65][2]; //2D ARRAY OF JBUTTONS        
-        
+       
         int n=1;      
 
+        //CREATING 2D BUTTONS AND ADDING THEM TO BOARD
 
         for (int i=0; i<8; i++)
          for (int j=0; j<8; j++)
@@ -42,7 +56,9 @@ public class Board {
             {    
              squares[n] = new Square(n,"whites","none");
              blocks[n][0]= squares[n].getBlock();
+             blocks[n][0].addActionListener(this);
              blocks[n][1]= squares[n].getBlock();
+             blocks[n][1].addActionListener(this);
              panel.add(blocks[n][1]);
              n++;
             }
@@ -50,8 +66,10 @@ public class Board {
             if (n<=24)
             {
              squares[n] = new Square(n,"whites","red");
-             blocks[n][0]= squares[n].getBlock();               
+             blocks[n][0]= squares[n].getBlock();
+             blocks[n][0].addActionListener(this);       
              blocks[n][1]= new JButton(red);
+             blocks[n][1].addActionListener(this);
              panel.add(blocks[n][1]);
              n++;
             } 
@@ -59,19 +77,73 @@ public class Board {
             if (n>=41)
             {
               squares[n] = new Square(n,"whites","white");
-              blocks[n][0]= squares[n].getBlock();               
+              blocks[n][0]= squares[n].getBlock(); 
+              blocks[n][0].addActionListener(this);             
               blocks[n][1]= new JButton(white);
+              blocks[n][1].addActionListener(this);
               panel.add(blocks[n][1]);
               n++;
-            }                   
-            
-         }         
-
+            }              
+         }  
         frame.setVisible(true);
-
     }
 
+    //ACTION PERFORMED (HIGHLIGHTING VALID MOVES)
 
+    public void actionPerformed(ActionEvent e)
+    {
+      for (int x=1; x<65; x++)
+       if (e.getSource()==blocks[x][1])
+       {
+          String color = squares[x].getCont();
+
+          String maybeMove= squares[x+7].getCont();
+          String background=squares[x+7].getbackgroung();
+          String maybeMove2= squares[x+9].getCont();
+          String background2=squares[x+9].getbackgroung();
+
+
+          if (color=="red")
+          {
+
+          if (!(x%8==1))
+            if(((maybeMove=="white") || (maybeMove=="none") && !(background=="black")))
+            {
+              blocks[x+7][1].setIcon(valid);
+            }
+
+          if (!(x%8==0))   
+            if(((maybeMove2=="white") || (maybeMove2=="none") && !(background2=="black")))
+            {
+              blocks[x+9][1].setIcon(valid);
+            }
+
+          }
+
+          String MaybeMove= squares[x-7].getCont();
+          String Background=squares[x-7].getbackgroung();
+          String MaybeMove2= squares[x-9].getCont();
+          String Background2=squares[x-9].getbackgroung();
+
+          if (color=="white")
+          {
+
+           if (!(x%8==0)) 
+            if(((MaybeMove=="red") || (MaybeMove=="none") && !(Background=="black")))
+            {
+              blocks[x-7][1].setIcon(valid);
+            }
+
+           if (!(x%8==1))   
+            if(((MaybeMove2=="red") || (MaybeMove2=="none") && !(Background2=="black")))
+            {
+              blocks[x-9][1].setIcon(valid);
+            }               
+
+          }
+       }      
+    }
+      
     public static void main(String[] args)
     {
         Board b = new Board();
--- Square.java
+++ Square.java
@@ -3,16 +3,39 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Square extends JPanel
+public class Square 
 {
+    //USED TO MAKE BUTTONS
+
+    JButton block= new JButton();
+    ImageIcon blacks = new ImageIcon("empty2.png");
+    ImageIcon whites = new ImageIcon("empty.png"); 
     
+    //INSTANCE VARIABLES
+
     int index;    
-    String cont;    
+    String cont;
+    String backgroundcolor;
+
+    public void setbackground(String c)
+    {
+        backgroundcolor=c;
+    }
+
+    public String getbackgroung()
+    {
+        return backgroundcolor;
+    }   
+
+    public String getCont()
+    {
+        return cont;
+    }    
 
     public void setcont(String c)
     {
         cont=c;
-    }
+    }    
 
     public void setIndex(int x)
     {
@@ -22,9 +45,7 @@ public class Square extends JPanel
     public int getIndex()
     {
         return index;
-    }
-
-    JButton block= new JButton();
+    }   
 
     public void setKey(ImageIcon c)
     {
@@ -33,12 +54,10 @@ public class Square extends JPanel
 
     public JButton getBlock()
     {
-        return block;
-    
-    }
+        return block;    
+    }          
 
-    ImageIcon blacks = new ImageIcon("empty2.png");
-    ImageIcon whites = new ImageIcon("empty.png");         
+    //CONSTRUCTOR
 
     public Square(int a, String c,String b)
     {
@@ -48,10 +67,10 @@ public class Square extends JPanel
         this.setKey(whites);
                 
         this.setIndex(a);
-        this.setcont(b);              
+        this.setcont(b);    
+        this.setbackground(c);   
 
     }
-
 
     public static void main(String[] args)
     {
